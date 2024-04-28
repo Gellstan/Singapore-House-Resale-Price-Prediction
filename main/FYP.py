7fd828f023edf2ff483980c52567702c683435e4
Gellstan <110098181+Gellstan@users.noreply.github.com>
2024-04-29 00:22:24 +0800

    .

--- main/FYP.py
+++ main/FYP.py
@@ -91,10 +91,10 @@ def prophet_predict(input_df):
     monthly_data = input_df['resale_price'].resample('M').mean()
     prophet_df = monthly_data.reset_index()
     prophet_df.columns = ['ds', 'y']
-    user_input_date = prophet_df['ds'].min()
-    end_date = pd.to_datetime('2030-12')
-    periods = (end_date.year - user_input_date.year) * 12 + (end_date.month - user_input_date.month)
-    future = prophet_model.make_future_dataframe(periods=periods, freq='M')
+    #user_input_date = prophet_df['ds'].min()
+    #end_date = pd.to_datetime('2030-12')
+    #periods = (end_date.year - user_input_date.year) * 12 + (end_date.month - user_input_date.month)
+    future = prophet_model.make_future_dataframe(periods=120, freq='M')
     prophet_prediction = prophet_model.predict(future)
     
     return prophet_prediction[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
