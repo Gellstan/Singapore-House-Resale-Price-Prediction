0013dc64a1855996ef7b49f91279c6e1c554eeed
Gellstan <110098181+Gellstan@users.noreply.github.com>
2024-04-29 00:28:36 +0800

    Update FYP.py

--- main/FYP.py
+++ main/FYP.py
@@ -101,7 +101,6 @@ def prophet_predict(input_df):
     return prophet_prediction[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
 def predicted_plot(unscaled_prophet_prediction):
-    unscaled_prophet_prediction['ds'] = unscaled_prophet_prediction['ds'].dt.tz_localize(None)
     fig, ax = plt.subplots(figsize=(10, 6))  # Set the figure size here
     ax.plot(origin_data['month'], origin_data['resale_price'], label='Historical', color='blue')
     ax.plot(unscaled_prophet_prediction['ds'], unscaled_prophet_prediction['predicted_value'], label='Predicted', color='orange')
